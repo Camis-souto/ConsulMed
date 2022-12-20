98871f567ad9b9d1b1cb1398573b06c8f7b35c8f
Marilia Santos Barbosa <msant.barbosa@gmail.com>
2022-12-20 15:07:29 -0300

    Adicionar arquivos de projeto.

---
+++ ConsulMed.Data/Interface/IBeneficiarioRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsulMed.Data;
+
+namespace ConsulMed.Data.Interface
+{
+    public interface IBeneficiarioRepositorio
+    {
+        List<Dto.BeneficiarioDto> ListarTodas();
+        Dto.BeneficiarioDto PorId(int idBeneficiario);
+        int Atualizar(Dto.BeneficiarioDto cadastrarDto);
+        int AtualizarId();
+        int Cadastrar(Dto.BeneficiarioDto cadastrarDto);
+        int Excluir(int idBeneficiario);
+    }
+}
---
+++ ConsulMed.Data/Repositorio/HospitalRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using ConsulMed.Data.Interface;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsulMed.Data.Repositorio
+{
+    public class HospitalRepositorio : IHospitalRepositorio
+    {
+    }
+}
